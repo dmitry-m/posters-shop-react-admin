a78ea0b6fc443e479f64c8e5681050253b58ecfd
dmitry-m <dmi1.martynov@gmail.com>
2024-05-01 19:28:59 +0500

    visitors number filed fix

--- src/visitors/ColoredNumberField.tsx
+++ src/visitors/ColoredNumberField.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
 import { useRecordContext, NumberField, NumberFieldProps } from "react-admin";
 
-delete NumberField.defaultProps;
-
 const ColoredNumberField = ({ source, ...props }: NumberFieldProps) => {
   const record = useRecordContext(props);
   if (!record || !source) {
     return null;
   }
   return record[source] > 500 ? (
-    <NumberField {...props} sx={{ color: "red" }} />
+    <NumberField {...props} source={source} sx={{ color: "red" }} />
   ) : (
-    <NumberField {...props} />
+    <NumberField {...props} source={source} />
   );
 };
-
-// ColoredNumberField.defaultProps = NumberField.defaultProps;
 
 export default ColoredNumberField;
